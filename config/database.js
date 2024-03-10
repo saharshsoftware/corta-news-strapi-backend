eefeb8e16e6a7300ceb949b419235715524cf101
Piyush Beli <piyush.beli@gmail.com>
2024-03-10 14:49:21 +0530

    Added sqlite also as a database client because I will use that for local

--- config/database.js
+++ config/database.js
@@ -1,29 +1,45 @@
+const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+
+  const connections = {
+    postgres: {
+      connection: {
+        host: env("DATABASE_HOST", "strapi-database.cneue8sw4uup.ap-northeast-1.rds.amazonaws.com"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "postgres123"),
+      },
+      pool: {
+        min: 0,
+        max: 4,
+        acquireTimeoutMillis: 300000,
+        createTimeoutMillis: 300000,
+        destroyTimeoutMillis: 300000,
+        idleTimeoutMillis: 30000,
+        reapIntervalMillis: 1000,
+        createRetryIntervalMillis: 2000
+      },
+    },
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          '..',
+          env('DATABASE_FILENAME', '.tmp/data.db')
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
     connection: {
-      host: env("DATABASE_HOST", "strapi-database.cneue8sw4uup.ap-northeast-1.rds.amazonaws.com"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "postgres123"),
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
-    useNullAsDefault: true,
-    ssl: {
-      rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), 
-    },
-
-    acquireConnectionTimeout: 1000000,
-    pool: {
-      min: 0,
-      max: 4,
-      acquireTimeoutMillis: 300000,
-      createTimeoutMillis: 300000,
-      destroyTimeoutMillis: 300000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 2000
-    },
-  },
-});
+  };
+};
