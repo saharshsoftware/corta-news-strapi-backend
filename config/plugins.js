e579b8aa42b7445c443a093ea000cea5a30563d4
Piyush Beli <piyush.beli@gmail.com>
2024-03-12 00:04:39 +0530

    Setup Amazon SES for sending the emails

--- config/plugins.js
+++ config/plugins.js
@@ -33,5 +33,19 @@ module.exports = ({ env }) => ({
         },
       }
     },
-  }
+  },
+  email: {
+    config: {
+      provider: 'amazon-ses',
+      providerOptions: {
+        key: env('AWS_SES_KEY'),
+        secret: env('AWS_SES_SECRET'),
+        amazon: 'https://email.us-east-1.amazonaws.com',
+      },
+      settings: {
+        defaultFrom: 'info@corta.news',
+        defaultReplyTo: 'info@corta.news',
+      },
+    },
+  },
 });
