a540125d30eae79ae6c50c6f5c98673be97d8a62
Piyush Beli <piyush.beli@gmail.com>
2024-12-16 10:38:18 +0530

    Updated the find news post API to return pagination data also. Limit the no of categories to up to 2

--- src/api/news-post/controllers/custom-news-post.js
+++ src/api/news-post/controllers/custom-news-post.js
@@ -1,6 +1,9 @@
 module.exports = {
     async fetchNews(ctx) {
-      const { country, categories, types, page = 1, pageSize = 40 } = ctx.query;
+      const { country, categories, types } = ctx.query;
+      let {page = 1, pageSize = 40 } = ctx.query;
+      page = parseInt(page, 10);
+      pageSize = parseInt(pageSize, 10);
   
       // Calculate 48-hour filter for publicationDate
       const now = new Date();
@@ -32,17 +35,38 @@ module.exports = {
         const limit = parseInt(pageSize, 10);
   
         // Fetch paginated results directly from the database
-        const news = await strapi.entityService.findMany('api::news-post.news-post', {
-          filters,
-          sort: [
-            { importanceScore: 'desc' },
-            { publicationDate: 'desc' },
-          ],
-          start, // Start index for pagination
-          limit, // Limit for pagination
+        const [news, total] = await Promise.all([
+          strapi.entityService.findMany('api::news-post.news-post', {
+            filters,
+            sort: [
+              { importanceScore: 'desc' },
+              { publicationDate: 'desc' },
+            ],
+            start,
+            limit,
+          }),
+          strapi.entityService.count('api::news-post.news-post', { filters }), // Get total count
+        ]);
+
+        // Limit the news categories upto 2 only
+        news.forEach((newsPost) => {
+          const categories = newsPost.categories.split(',');
+          if (categories.length > 2) {
+            newsPost.categories = categories.slice(0, 2).join(',');
+          }
         });
-  
-        return news;
+      
+        // Prepare metadata
+        const meta = {
+          pagination: {
+            total,
+            page,
+            pageSize,
+            pageCount: Math.ceil(total / pageSize),
+          }
+        };
+      
+        return { data: news, meta };
       } catch (err) {
         ctx.throw(500, err.message);
       }
